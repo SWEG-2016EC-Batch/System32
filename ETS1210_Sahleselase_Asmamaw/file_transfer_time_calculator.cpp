bedf4688bbcfe5a2a05fd09939cee01332016297
kenpachi <sahleselase.asmamaw@aastustudent.edu.et>
2024-11-29 22:08:47 +0300

    Update file_transfer_time_calculator.cpp

--- ETS1210_Sahleselase_Asmamaw/file_transfer_time_calculator.cpp
+++ ETS1210_Sahleselase_Asmamaw/file_transfer_time_calculator.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int file_size,transfer_rate=960,total_time,days,minutes,hours,seconds,total_seconds;
+    double file_size,transfer_rate=960,total_time,days,minutes,hours,seconds,total_seconds;
 cout<<"Enter the file size in bytes: \n";
 cin>>file_size;
 /*file_size=419430400;*/
