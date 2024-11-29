1af15e320fed6e4ed8287e9ad20f716120d6268b
Sami siraj nurhussein <sami500554@gmail.com>
2024-11-29 15:25:46 +0300

    Create x^y.cpp

---
+++ Ets1214 Sami siraj  nurhussein/x^y.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+ float x;
+ float y;
+ float power;
+  cout <<"Enter x: ";
+  cin>>x;
+  cout <<"Enter y: ";
+  cin>>y;
+  power=pow(x,y);
+  cout<<"x the power of y is: "<<power;
+return 0 ;
+}
