3f31dc3d40372e9b5c7f6c030a7ded954f980b7f
Sami siraj nurhussein <sami500554@gmail.com>
2024-11-29 22:13:50 +0300

    Update Automobile tank.cpp

--- Ets1214 Sami siraj  nurhussein/Automobile tank.cpp
+++ Ets1214 Sami siraj  nurhussein/Automobile tank.cpp
@@ -1,14 +1,11 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-    float  tankCapacity;
-    float milesPerGallon;
-    cout << "Enter the capacity of the fuel tank (in gallons): ";
-    cin >> tankCapacity;
-    cout << "Enter the miles per gallon (MPG): ";
-    cin >> milesPerGallon;
-     float totalDistance = tankCapacity * milesPerGallon;
-    cout << "The automobile can be driven for " << totalDistance << " miles without refueling." << endl;
-
-    return 0;
+int main(){
+float gallons,milesPerGallon,numberOfMiles;
+cout<<"Enter the fuel capacity of the automobile in gallons: \n";
+cin>>gallons;
+cout<<"Enter the miles per gallon the automon=bile can travel: \n";
+cin>>milesPerGallon;
+numberOfMiles=gallons*milesPerGallon;
+cout<<"The automobile can travel for "<<numberOfMiles<<" miles.";
 }
