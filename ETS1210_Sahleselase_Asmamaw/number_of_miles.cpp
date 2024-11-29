3eab4a2a817e2133655e6dd4a7854bb77fb87a5c
kenpachi <sahleselase.asmamaw@aastustudent.edu.et>
2024-11-29 21:54:35 +0300

    Update number_of_miles.cpp

--- ETS1210_Sahleselase_Asmamaw/number_of_miles.cpp
+++ ETS1210_Sahleselase_Asmamaw/number_of_miles.cpp
@@ -4,7 +4,7 @@ int main(){
 float gallons,milesPerGallon,numberOfMiles;
 cout<<"Enter the fuel capacity of the automobile in gallons: \n";
 cin>>gallons;
-cout<<"Enter the miles per gallon the automon=bile can travel: \n";
+cout<<"Enter the miles per gallon the automobile can travel: \n";
 cin>>milesPerGallon;
 numberOfMiles=gallons*milesPerGallon;
 cout<<"The automobile can travel for "<<numberOfMiles<<" miles.";
