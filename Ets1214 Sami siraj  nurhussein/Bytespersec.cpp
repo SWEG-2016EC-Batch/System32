8b2007d1a5c1566cce5b070eded68d608921d65a
Sami siraj nurhussein <sami500554@gmail.com>
2024-11-29 17:55:02 +0300

    Create Bytespersec.cpp

---
+++ Ets1214 Sami siraj  nurhussein/Bytespersec.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <limits> 
+#include <iomanip> 
+
+using namespace std;
+
+int main() {
+    const int CHAR_SIZE = 1; 
+    const int TRANSMISSION_RATE = 960; 
+    double fileSize;
+    while (true) {
+        cout << "Enter the file size in bytes: ";
+        cin >> fileSize;
+        if (cin.fail() || fileSize < 0) {
+            cout << "File size cannot be negative or invalid. Please enter a valid file size." << endl;
+            cin.clear(); 
+            cin.ignore();
+        } 
+        else {
+            break; 
+        }
+    }
+    double fileSizeCharacters = fileSize / CHAR_SIZE;
+    double timeSeconds = fileSizeCharacters / TRANSMISSION_RATE;
+    cout << fixed; 
+    cout << "Time to transmit the file: " << timeSeconds << " seconds" << endl;
+
+    return 0;
+}
