f06015391fa07a592e1a701247a707f08529e872
Makda Solomon <makdas205@gmail.com>
2024-11-30 13:53:14 +0300

    Update BMI_calculator.cpp

--- ETS0892_Makda_Solomon/BMI_calculator.cpp
+++ ETS0892_Makda_Solomon/BMI_calculator.cpp
@@ -1,40 +1,36 @@
-#include <iostream>
+#include<iostream>
+#include<cmath>
 using namespace std;
+int main(){
+  int num;
 
-int main() {
-    float weight, height, bmi;
-    char response;
-
-    do {
-        // Input weight
-        cout << "Enter weight in kg: ";
-        cin >> weight;
-
-        // Input height
-        cout << "Enter height in meters: ";
-        cin >> height;
-
-        // Calculate BMI
-        bmi = weight / (height * height);
-
-        // Determine weight status
-        string status;
-        if (bmi < 18.5) {
-            status = "Underweight";
-        } else if (bmi >= 18.5 && bmi < 24.9) {
-            status = "Normal weight";
-        } else {
-            status = "Overweight";
+  cout <<" Enter the number of people for BMI calculation:" << endl;
+  cin>>num;
+  for (int i=1;i<=num;++i){
+        float weight,height,bmi;
+     
+      cout << "\nPerson " << i << ":\n";
+      
+      cout<<"Enter your weight(kg): "<<endl;
+      cin>>weight;
+      
+      cout<<"Enter your height(m): "<<endl;
+      cin>>height;
+     
+      bmi=weight/(height*height);
+       cout<<"Your BMI is: "<<bmi<<endl;
+      
+      if (bmi < 18.5) {
+            cout << "You are Underweight!" << endl;
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+            cout << "You have a Normal weight!" << endl;
+        } else if (bmi >= 25 && bmi < 29.9) {
+            cout << "You are Overweight!" << endl;
+        } else if (bmi >= 30) {
+            cout << "You are Obese!" << endl;
         }
-
-        // Output BMI and status
-        cout << "BMI: " << bmi << " Status: " << status << endl;
-
-        // Ask if user wants to continue
-        cout << "Do you want to enter details for another person? (y/n): ";
-        cin >> response;
-
-    } while (response == 'y' || response == 'Y');
-
-    return 0;
-}
+    }
+  
+  return 0;
+ 
+  }
