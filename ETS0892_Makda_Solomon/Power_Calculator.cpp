cd9533619736d24d4bc828d637dd8011e5dc1309
Makda Solomon <makdas205@gmail.com>
2024-11-30 18:45:19 +0300

    Update Power_Calculator.cpp

--- ETS0892_Makda_Solomon/Power_Calculator.cpp
+++ ETS0892_Makda_Solomon/Power_Calculator.cpp
@@ -1,21 +1,19 @@
-#include <iostream>
-#include <cmath> //  for the pow function
+#include<iostream>
 using namespace std;
-
-int main() {
-    float x, y, result;
-
-    cout << "Enter the value of x: ";
-    cin >> x;
-
-    cout << "Enter the value of y: ";
-    cin >> y;
-
-
-    result = pow(x, y);
-
-
-    cout << x << " raised to the power of " << y << " is: " << result << endl;
-
-    return 0;
+int main(){
+    int x,y,result=1;
+    cout<<"Enter the base number:\n";
+    cin>>x;
+    cout<<"Enter the Exponent number:\n";
+    cin>>y;
+    if(cin.fail() ||y<0){
+        cout<<"Please enter a positive integer for the exponent";
+        }
+    else{
+        for(int i=0;i<y;i++){
+            result*=x;
+        }
+     cout<<x<<" raised to "<<y<<" is "<<result;
+    }    
+   
 }
