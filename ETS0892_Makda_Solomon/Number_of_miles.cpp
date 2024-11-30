7f96dcee7e1b5f75a837033b04f4468362856d3f
Makda Solomon <makdas205@gmail.com>
2024-11-30 16:29:09 +0300

    Update Number_of_miles.cpp

--- ETS0892_Makda_Solomon/Number_of_miles.cpp
+++ ETS0892_Makda_Solomon/Number_of_miles.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+  
+    float capacityGallons;
+    float milesPerGallon;
+
+    
+    cout << "Enter the capacity of the fuel tank in gallons: ";
+    cin >> capacityGallons;
+
+    
+    cout << "Enter the miles per gallon (MPG): ";
+    cin >> milesPerGallon;
+
+    double totalMiles = capacityGallons * milesPerGallon;
+
+    
+    cout << "The automobile can be driven " << totalMiles << " miles without refueling." << endl;
+
+    return 0;
+}
