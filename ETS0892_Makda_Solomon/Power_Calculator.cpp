ba0a2b72248095ab99354c5ce42bd4d932b13ded
Makda Solomon <makdas205@gmail.com>
2024-11-30 17:31:25 +0300

    Update Power_Calculator.cpp

--- ETS0892_Makda_Solomon/Power_Calculator.cpp
+++ ETS0892_Makda_Solomon/Power_Calculator.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cmath> //  for the pow function
+using namespace std;
 
+int main() {
+    float x, y, result;
+
+    cout << "Enter the value of x: ";
+    cin >> x;
+
+    cout << "Enter the value of y: ";
+    cin >> y;
+
+
+    result = pow(x, y);
+
+
+    cout << x << " raised to the power of " << y << " is: " << result << endl;
+
+    return 0;
+}
