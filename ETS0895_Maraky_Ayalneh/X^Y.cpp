96a1e614f87b966924056ebe35c724b4c92ff1f6
Maraky Ayalneh <amsalyayerad@gmail.com>
2024-12-01 20:32:44 +0300

    Update X^Y.cpp

--- ETS0895_Maraky_Ayalneh/X^Y.cpp
+++ ETS0895_Maraky_Ayalneh/X^Y.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std ;
+int main() {
+    float x, y;
+    cout << "Enter the base (x): ";
+    cin >> x;
+    cout << "Enter the exponent (y): ";
+    cin >> y;
 
+    float result = 1;
+     if (y >0) {
+        for (int i = 0; i < y; ++i) {
+        result *= x;
+        }
+    }
+  else if (y<0){
+        for (int i = 0; i <-y; ++i) {
+            result *= x;
+        }
+        result = 1 / result;
+    }
+else { result= 1; }
+    cout << x << " raised to the power " << y << " is " << result << endl;
+    return 0;
+    }
