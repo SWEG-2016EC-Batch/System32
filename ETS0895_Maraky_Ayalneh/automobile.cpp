1ef85f48b7acbc598572d8a050deddafe42d840c
Maraky Ayalneh <amsalyayerad@gmail.com>
2024-12-01 22:02:59 +0300

    Update automobile.cpp

--- ETS0895_Maraky_Ayalneh/automobile.cpp
+++ ETS0895_Maraky_Ayalneh/automobile.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    
+    float tankCapacity, miles, totalDistance;
+
+    cout << "Enter the capacity of the fuel tank in gallons: ";
+    cin >> tankCapacity;
+    
+    cout << "Enter the miles per gallon the automobile can be driven: ";
+    cin >> miles;
+
+    totalDistance = tankCapacity * miles;
+    
+    cout << "The automobile can be driven " << totalDistance << " miles without refueling." << endl;
+
+    return 0;
+}
