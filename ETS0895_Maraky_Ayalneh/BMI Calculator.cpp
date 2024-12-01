2f6197d5f96464fd2ae2b0456e12823ecffa0d53
Maraky Ayalneh <amsalyayerad@gmail.com>
2024-12-01 19:55:43 +0300

    Update BMI Calculator.cpp

--- ETS0895_Maraky_Ayalneh/BMI Calculator.cpp
+++ ETS0895_Maraky_Ayalneh/BMI Calculator.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+float weight, height, bmi;
+char choice;
+
+do {
+
+    cout << "Enter your weight in kilograms: ";
+    cin >> weight;
+    cout << "Enter your height in meters: ";
+    cin >> height;
+
+    bmi = weight / (height * height);
+
+    cout << "Your BMI is: " << bmi << endl;
+
+    if (bmi < 18.5) {
+        cout << "You are underweight." << endl;
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        cout << "You have a normal weight." << endl;
+    } else if (bmi >= 25 && bmi < 29.9) {
+        cout << "You are overweight." << endl;
+    } else {
+        cout << "You are obese." << endl;
+    }
+    cout << "Do you want to continue (y/n): ";
+    cin >> choice;
+
+} while (choice == 'y' || choice == 'Y');
+
+return 0;
+}
