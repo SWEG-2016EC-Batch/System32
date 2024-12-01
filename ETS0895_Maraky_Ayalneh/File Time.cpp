1e6c13073807a349500fd3bcbbd010c23da848d9
Maraky Ayalneh <amsalyayerad@gmail.com>
2024-12-02 00:38:15 +0300

    Update File Time.cpp

--- ETS0895_Maraky_Ayalneh/File Time.cpp
+++ ETS0895_Maraky_Ayalneh/File Time.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    const int transmissionSpeed = 960; 
+    long long fileSize, time, days, hours, minutes, seconds;
+
+    cout << "Enter the file size in bytes: ";
+    cin >> fileSize;
+
+    time = fileSize / transmissionSpeed;
+    
+    days = time / 86400; 
+    hours = (time % 86400) / 3600; 
+    minutes = (time % 3600) / 60;  
+    seconds = time % 60;          
+
+    cout << "Time to send the file: " << days << " days, "
+         << hours << " hours, " << minutes << " minutes, and "
+         << seconds << " seconds." << endl;
+
+    return 0;
+}
