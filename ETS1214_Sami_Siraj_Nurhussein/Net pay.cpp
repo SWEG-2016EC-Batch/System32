58db9025a8e6f4271e68ba81f30423036c19ed0c
Sami siraj nurhussein <sami500554@gmail.com>
2024-12-09 22:44:30 +0300

    Update Net pay.cpp

--- ETS1214_Sami_Siraj_Nurhussein/Net pay.cpp
+++ ETS1214_Sami_Siraj_Nurhussein/Net pay.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    float basicSalary, workedHours, bonusRate, overtimeBonusRate = 0.0;
-    float pensionRate = 0.07, taxRate, grossPay, netPay;
+    double basicSalary, workedHours, bonusRate, overtimeBonusRate = 0.0;
+    double grossPay, netPay, pensionDeduction, taxDeduction;
+    const double pensionRate = 0.07; 
     cout << "Enter the basic salary: ";
     cin >> basicSalary;
     cout << "Enter the worked hours: ";
@@ -13,14 +15,26 @@ int main() {
         cout << "Enter the overtime bonus rate per hour: ";
         cin >> overtimeBonusRate;
     }
-    cout << "Enter the tax rate (as a decimal, e.g., 0.15 for 15%): ";
-    cin >> taxRate;
     grossPay = basicSalary + (workedHours * bonusRate);
     if (workedHours > 40) {
         grossPay += (workedHours - 40) * overtimeBonusRate;
     }
-    double pensionDeduction = grossPay * pensionRate;
-    double taxDeduction = grossPay * taxRate;
+    pensionDeduction = grossPay * pensionRate;
+    if (grossPay <= 200) {
+        taxDeduction = 0;
+    } else if (grossPay <= 600) {
+        taxDeduction = (grossPay - 200) * 0.10;
+    } else if (grossPay <= 1200) {
+        taxDeduction = (400 * 0.10) + (grossPay - 600) * 0.15;
+    } else if (grossPay <= 2000) {
+        taxDeduction = (400 * 0.10) + (600 * 0.15) + (grossPay - 1200) * 0.20;
+    } else if (grossPay <= 3500) {
+        taxDeduction = (400 * 0.10) + (600 * 0.15) + (800 * 0.20) + (grossPay - 2000) * 0.25;
+    } else {
+        taxDeduction = (400 * 0.10) + (600 * 0.15) + (800 * 0.20) + (1500 * 0.25) + (grossPay - 3500) * 0.30;
+    }
     netPay = grossPay - pensionDeduction - taxDeduction;
     cout << "The Net-Pay of the employee is: " << netPay << endl;
+
+    return 0;
 }
