945a898d9ec203cef1c1b9dbfe9d1f2b2d389622
Makda Solomon <makdas205@gmail.com>
2024-12-13 09:03:19 +0300

    Update Net_Pay.cpp

--- ETS0892_Makda_Solomon/Net_Pay.cpp
+++ ETS0892_Makda_Solomon/Net_Pay.cpp
@@ -2,33 +2,39 @@
 using namespace std;
 
 int main() {
-    double basic_salary, worked_hours, bonus_rate, tax_rate;
-    double pension, tax, overtime = 0, net_pay;
-
-    
-    cout << "Enter basic salary: ";
-    cin >> basic_salary;
-
-    cout << "Enter worked hours: ";
-    cin >> worked_hours;
-
-    cout << "Enter bonus rate: ";
-    cin >> bonus_rate;
-    pension = basic_salary * 0.07;
-
-    cout << "Enter tax rate (as a percentage): ";
-    cin >> tax_rate;
-    tax = (basic_salary - pension) * (tax_rate / 100);
-
-    if (worked_hours > 40) {
-        double overtime_rate;
-        cout << "Enter overtime bonus rate per hour: ";
-        cin >> overtime_rate;
-        overtime = (worked_hours - 40) * overtime_rate;
+    double basicSalary, workedHours, bonusRate, overtimeBonusRate = 0.0;
+    double grossPay, netPay, pensionDeduction, taxDeduction;
+    const double pensionRate = 0.07; 
+    cout << "Enter the basic salary: ";
+    cin >> basicSalary;
+    cout << "Enter the worked hours: ";
+    cin >> workedHours;
+    cout << "Enter the bonus rate: ";
+    cin >> bonusRate;
+    if (workedHours > 40) {
+        cout << "Enter the overtime bonus rate per hour: ";
+        cin >> overtimeBonusRate;
     }
-    net_pay = basic_salary - pension - tax + bonus_rate + overtime;
-
-     cout << "Net Pay: " << net_pay << endl;
+    grossPay = basicSalary + (workedHours * bonusRate);
+    if (workedHours > 40) {
+        grossPay += (workedHours - 40) * overtimeBonusRate;
+    }
+    pensionDeduction = grossPay * pensionRate;
+    if (grossPay <= 200) {
+        taxDeduction = 0;
+    } else if (grossPay <= 600) {
+        taxDeduction = (grossPay - 200) * 0.10;
+    } else if (grossPay <= 1200) {
+        taxDeduction = (400 * 0.10) + (grossPay - 600) * 0.15;
+    } else if (grossPay <= 2000) {
+        taxDeduction = (400 * 0.10) + (600 * 0.15) + (grossPay - 1200) * 0.20;
+    } else if (grossPay <= 3500) {
+        taxDeduction = (400 * 0.10) + (600 * 0.15) + (800 * 0.20) + (grossPay - 2000) * 0.25;
+    } else {
+        taxDeduction = (400 * 0.10) + (600 * 0.15) + (800 * 0.20) + (1500 * 0.25) + (grossPay - 3500) * 0.30;
+    }
+    netPay = grossPay - pensionDeduction - taxDeduction;
+    cout << "The Net-Pay of the employee is: " << netPay << endl;
 
     return 0;
 }
